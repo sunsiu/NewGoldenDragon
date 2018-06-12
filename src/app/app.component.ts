fee393850ea58ee6185cfa762d1c6f6b7c3f5abe
Sunsiu <sunnytsiu@gmail.com>
2018-06-12 00:17:58 -0600

    Created Home page and routing for most buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:`
-  <div id="whole-page">
-    <div id="outer-menu">
-      <div class="table">
-        <ul id="nav-list">
-            <li><button mat-button><a [routerLink]="['/home']">Home</a></button></li>
-            <li><button mat-button>Menu</button></li>
-            <li><button mat-button>Location</button></li>
-            <li><button mat-button><a [routerLink]="['/contact']">Contact</a></button></li>
-        </ul>
-      </div>
-    </div>
-    <div>
-      <router-outlet></router-outlet>
-    </div>
-  </div>
-  `,
-  styleUrls: ['./pages/home-page.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'New Golden Dragon';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page.component';
@@ -13,12 +13,14 @@ import { ContactPageComponent } from './pages/contact-page.component';
     HomePageComponent,
     ContactPageComponent
   ],
+
   imports: [
     BrowserModule,
     MatButtonModule,
     RouterModule.forRoot([
       { path: 'home', component: HomePageComponent },
       { path: 'contact', component: ContactPageComponent},
+      { path: 'menu', redirectTo: "assets/images/Menu.pdf", pathMatch: "full"},
       { path: '', redirectTo: 'home', pathMatch: 'full'}
       //add notfound page
     ])
